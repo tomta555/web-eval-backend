dee0ec4e78065379074845094f4dd48831129ffd
Thanawong <thanawong.saneewong@gmail.com>
2021-03-04 00:07:00 +0700

    fix api

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 
 // app.use('/public', express.static(path.join(__dirname,'/public')))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'https://affectionate-roentgen-01a06a.netlify.app/timetable')
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
     next()
